7163dbcf19c6fdfe28f7d65ef5905cbc01c05788
anak10thn <anak10thn@gmail.com>
2018-02-14 17:39:46 +0700

    ADD: list images #17

---
+++ neo/libs/image.py
@@ -0,0 +1,12 @@
+from neo.libs import login as login_lib
+from glanceclient import Client as image_client
+
+
+def get_image_client():
+    img = image_client('2', session=login_lib.get_session())
+    return img
+
+
+def get_list():
+    img = get_image_client()
+    return img.images.list()
