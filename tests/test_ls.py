db3b6cd01aa8b9315958578a5146eef6f2bdcea9
azzamsa <oktinastuzi@gmail.com>
2018-07-20 13:44:07 +0700

    ADD: test ls without argument

--- tests/test_ls.py
+++ tests/test_ls.py
@@ -1,6 +1,9 @@
 """Tests for `neo ls` subcommand."""
 
 import pytest
+import os
+from io import StringIO
+from contextlib import redirect_stdout
 from subprocess import PIPE, Popen as popen
 from neo.clis import Ls
 
@@ -29,3 +32,12 @@ class TestLs:
             a = Ls({'<args>': ['-o', 'referensi-vm'], '<command>': 'ls'},
                    '-o', 'referensi-vm')
             popen(a.execute(), stdout=PIPE)
+
+    def test_ls(self):
+        os.chdir("tests")
+        f = StringIO()
+        with redirect_stdout(f):
+            a = Ls({'<args>': [], '<command>': 'ls'}, None)
+            foo = a.execute()
+        out = f.getvalue()
+        assert 'unittest-vm' in out
