3fc47a211100c4a492181cb9d94185ccd5a5260f
azzamsa <oktinastuzi@gmail.com>
2018-07-20 13:48:18 +0700

    Fix: no need subprocess

--- tests/test_ls.py
+++ tests/test_ls.py
@@ -13,17 +13,17 @@ class TestLs:
     def test_ls_vm(self):
         with pytest.raises(SystemExit):
             a = Ls({'<command>': 'ls'}, 'vm')
-            popen(a.execute(), stdout=PIPE)
+            a.execute()
 
     def test_ls_stack(self):
         with pytest.raises(SystemExit):
             a = Ls({'<command>': 'ls'}, 'stack')
-            popen(a.execute(), stdout=PIPE)
+            a.execute()
 
     def test_ls_net(self):
         with pytest.raises(SystemExit):
             a = Ls({'<command>': 'ls'}, 'network')
-            popen(a.execute(), stdout=PIPE)
+            a.execute()
 
     def test_ls_output(self):
         # no exit(). but failed when calle without
@@ -31,7 +31,7 @@ class TestLs:
         with pytest.raises(SystemExit):
             a = Ls({'<args>': ['-o', 'referensi-vm'], '<command>': 'ls'},
                    '-o', 'referensi-vm')
-            popen(a.execute(), stdout=PIPE)
+            a.execute()
 
     def test_ls(self):
         os.chdir("tests")
