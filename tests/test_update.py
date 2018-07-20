75ea0c03764125d4926394497f98ed75d1977401
azzamsa <oktinastuzi@gmail.com>
2018-07-20 14:25:52 +0700

    Fix: use funcall directly with docopt args

--- tests/test_update.py
+++ tests/test_update.py
@@ -5,7 +5,7 @@ import os
 import time
 from neo.libs import vm as vm_lib
 from neo.libs import orchestration as orch
-
+from neo.clis import Update
 
 class TestUpdate:
     @pytest.mark.run(order=2)
@@ -24,11 +24,11 @@ class TestUpdate:
             time.sleep(2)
             print('waiting until vm activated ...')
 
-        deploy_init = orch.initialize(cwd + "/tests/neo2.yml")
-        orch.do_update(deploy_init)
+        a = Update({'<args>': ['-f', 'tests/neo2.yml'],
+                    '<command>': 'update'}, '-f', 'tests/neo2.yml')
+        a.execute()
         print(vm_name + ' updated')
 
-        # check updated vm
         # wait until successfully updated
         updated_status = None
         while updated_status == None:
