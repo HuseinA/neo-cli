926287ef0bfb84526896aa77e3a49bb4e7451679
azzamsa <oktinastuzi@gmail.com>
2018-07-26 14:49:56 +0700

    ADD: exception test for ls

--- tests/test_ls.py
+++ tests/test_ls.py
@@ -41,3 +41,28 @@ class TestLs:
         out = f.getvalue()
         os.chdir(os.pardir)
         assert 'unittest-vm' in out
+
+    def test_ls_vm_exception(self, monkeypatch):
+        with pytest.raises(SystemExit):
+            def mockreturn():
+                return []
+            monkeypatch.setattr("neo.libs.vm.get_list", mockreturn)
+            a = Ls({'<command>': 'ls'}, 'vm')
+            a.execute()
+
+    def test_ls_network_exception(self, monkeypatch):
+        with pytest.raises(SystemExit):
+            def mockreturn():
+                return []
+            monkeypatch.setattr("neo.libs.network.get_list", mockreturn)
+            a = Ls({'<command>': 'ls'}, 'network')
+            a.execute()
+
+    def test_ls_output_exception(self, monkeypatch):
+        with pytest.raises(SystemExit):
+            def mockreturn():
+                return []
+            monkeypatch.setattr("neo.libs.utils.get_project", mockreturn)
+            a = Ls({'<args>': ['-o', 'unittest-vm'], '<command>': 'ls'},
+                   '-o', 'unittest-vm')
+            a.execute()
