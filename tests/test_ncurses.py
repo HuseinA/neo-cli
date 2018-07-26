e9668132a0a2782f9c0a0f68b01f561f45a8b879
azzamsa <oktinastuzi@gmail.com>
2018-07-26 14:13:31 +0700

    ADD: patch more npyscreen functions

--- tests/test_ncurses.py
+++ tests/test_ncurses.py
@@ -3,7 +3,6 @@ from neo.libs import ncurses
 
 
 class TestCurses:
-
     def test_curses_get_stack(self, monkeypatch):
         def mockreturn():
             return 'foo'
@@ -16,4 +15,32 @@ class TestCurses:
             return 'foo'
         monkeypatch.setattr("neo.libs.ncurses.get_project", mockreturn)
         x = ncurses.get_project()
+        assert x == 'foo'
+
+    def test_curses_setup_form(self, monkeypatch):
+        def mockreturn():
+            return 'foo'
+        monkeypatch.setattr("neo.libs.ncurses.setup_form", mockreturn)
+        x = ncurses.setup_form()
+        assert x == 'foo'
+
+    def test_curses_exec_form(self, monkeypatch):
+        def mockreturn():
+            return 'foo'
+        monkeypatch.setattr("neo.libs.ncurses.exec_form", mockreturn)
+        x = ncurses.exec_form()
+        assert x == 'foo'
+
+    def test_curses_dump(self, monkeypatch):
+        def mockreturn():
+            return 'foo'
+        monkeypatch.setattr("neo.libs.ncurses.dump", mockreturn)
+        x = ncurses.dump()
+        assert x == 'foo'
+
+    def test_curses_init(self, monkeypatch):
+        def mockreturn():
+            return 'foo'
+        monkeypatch.setattr("neo.libs.ncurses.init", mockreturn)
+        x = ncurses.init()
         assert x == 'foo'
