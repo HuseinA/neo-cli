8fbb65ad3236e55f9c8377ef692fbc4e263924f1
azzamsa <oktinastuzi@gmail.com>
2018-08-31 22:21:48 +0700

    Add: wrap ls vm inside try-catch

--- neo/clis/ls.py
+++ neo/clis/ls.py
@@ -43,39 +43,43 @@ Run 'neo ls COMMAND --help' for more information on a command.
         default_file = orch.check_manifest_file()
 
         if self.args["vm"]:
-            data_instance = list()
-            for instance in vm_lib.get_list():
-                pre_instance = [instance.id, instance.name, instance.key_name]
-                pre_instance.append(image.detail(instance.image["id"]).name)
-                flavors = vm_lib.detail_flavor(instance.flavor["id"])
-                flavors_name = flavors.name
-                flavors_vcpu = flavors.vcpus
-                flavors_ram = bitmath.MiB(flavors.ram).to_GiB().best_prefix()
-                pre_instance.append(flavors_name)
-                pre_instance.append(flavors_ram)
-                pre_instance.append(flavors_vcpu)
+            try:
+                data_instance = list()
+                for instance in vm_lib.get_list():
+                    pre_instance = [instance.id, instance.name, instance.key_name]
+                    pre_instance.append(image.detail(instance.image["id"]).name)
+                    flavors = vm_lib.detail_flavor(instance.flavor["id"])
+                    flavors_name = flavors.name
+                    flavors_vcpu = flavors.vcpus
+                    flavors_ram = bitmath.MiB(flavors.ram).to_GiB().best_prefix()
+                    pre_instance.append(flavors_name)
+                    pre_instance.append(flavors_ram)
+                    pre_instance.append(flavors_vcpu)
 
-                # Address
-                addr = list()
-                addr_objs = utils.get_index(instance.addresses)
-                if len(addr_objs) > 0:
-                    for addr_obj in addr_objs:
-                        addr.append("network : {}".format(addr_obj))
-                        for addr_ip in instance.addresses[addr_obj]:
-                            addr_meta = "{} IP : {}".format(
-                                addr_ip["OS-EXT-IPS:type"], addr_ip["addr"])
-                            addr.append(addr_meta)
-                if len(addr) > 0:
-                    pre_instance.append("\n".join(addr))
-                else:
-                    pre_instance.append("")
+                    # Address
+                    addr = list()
+                    addr_objs = utils.get_index(instance.addresses)
+                    if len(addr_objs) > 0:
+                        for addr_obj in addr_objs:
+                            addr.append("network : {}".format(addr_obj))
+                            for addr_ip in instance.addresses[addr_obj]:
+                                addr_meta = "{} IP : {}".format(
+                                    addr_ip["OS-EXT-IPS:type"], addr_ip["addr"])
+                                addr.append(addr_meta)
+                    if len(addr) > 0:
+                        pre_instance.append("\n".join(addr))
+                    else:
+                        pre_instance.append("")
 
-                pre_instance.append(instance.status)
-                data_instance.append(pre_instance)
+                    pre_instance.append(instance.status)
+                    data_instance.append(pre_instance)
 
-            if len(data_instance) == 0:
-                utils.log_err("No Data...")
-                print(self.__doc__)
+                if len(data_instance) == 0:
+                    utils.log_err("No Data...")
+                    print(self.__doc__)
+
+            except Exception as e:
+                utils.log_err(e)
                 exit()
             print(
                 tabulate(
