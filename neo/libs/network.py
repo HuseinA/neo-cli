7dd933b6fff3523ce3225ccc554ad56b583624db
Sofyan Saputra <meongbego@gmail.com>
2018-09-17 22:41:59 +0700

    Add: Handling Error Network API

--- neo/libs/network.py
+++ neo/libs/network.py
@@ -1,5 +1,6 @@
 from neo.libs import login as login_lib
 from neutronclient.v2_0 import client as neutron_client
+from neo.libs import utils
 
 
 def get_neutron_client(session=None):
@@ -11,90 +12,136 @@ def get_neutron_client(session=None):
 
 def get_list(session=None):
     neutron = get_neutron_client(session)
-    networks = neutron.list_networks()
+    try:
+        networks = neutron.list_networks()
+    except Exception as e:
+        utils.log_err(e)
     return networks['networks']
 
 
 def get_floatingips(session=None):
     neutron = get_neutron_client(session)
-    floatingips = neutron.list_floatingips()
+    try:
+        floatingips = neutron.list_floatingips()
+    except Exception as e:
+        utils.log_err(e)
     return floatingips['floatingips']
 
 
 def do_delete(network_id, session=None):
     neutron = get_neutron_client(session)
-    neutron.delete_network(network_id)
+    try:
+        neutron.delete_network(network_id)
+    except Exception as e:
+        utils.log_err(e)
 
 
 def list_sec_group(session=None):
     neutron = get_neutron_client(session)
-    sec_group =  neutron.list_security_groups()
+    try:
+        sec_group =  neutron.list_security_groups()
+    except Exception as e:
+        utils.log_err(e)
     return sec_group['security_groups']
 
 
 def rules_sec_groups(sec_group, session=None):
     obj_sec_rule = list()
     neutron = get_neutron_client(session)
-    sec_group =  neutron.list_security_groups()
-    sec_group = sec_group['security_groups']
-    for i in sec_group:
-        data = {
-            'name': i['name'],
-            'description': i['description']
-        }
-        obj_sec_rule.append(data)
-    return obj_sec_rule
+    try:
+        sec_group =  neutron.list_security_groups()['security_groups']
+    except Exception as e:
+        utils.log_err(e)
+    else:
+        for i in sec_group:
+            data = {
+                'name': i['name'],
+                'description': i['description']
+            }
+            obj_sec_rule.append(data)
+        return obj_sec_rule
 
 
 def list_subnet(session=None):
     obj_subnet_list = list()
     neutron = get_neutron_client(session)
-    obj_subnet_list = neutron.list_subnets()
+    try:
+        obj_subnet_list = neutron.list_subnets()
+    except Exception as e:
+        utils.log_err(e)
     return obj_subnet_list
 
 
 def show_subnet(subnet, session=None):
     obj_subnet = list()
     neutron = get_neutron_client(session)
-    obj_subnet = neutron.show_subnet(subnet)
+    try:
+        obj_subnet = neutron.show_subnet(subnet)
+    except Exception as e:
+        utils.log_err(e)
     return obj_subnet
 
 def delete_subnet(subnet, session):
     neutron = get_neutron_client(session)
-    return neutron.delete_subnet(subnet)
+    try:
+        return neutron.delete_subnet(subnet)
+    except Exception as e:
+        utils.log_err(e)
 
 
 def list_router(session=None):
     obj_router_list = list()
     neutron = get_neutron_client(session)
-    obj_router_list = neutron.list_routers()
+    try:
+        obj_router_list = neutron.list_routers()
+    except Exception as e:
+        utils.log_err(e)
     return obj_router_list
 
 
 def show_router(routers, session=None):
     neutron = get_neutron_client(session)
-    obj_router = neutron.show_router(routers)
+    try:
+        obj_router = neutron.show_router(routers)
+    except Exception as e:
+        utils.log_err(e)
+
     return obj_router
 
 
 def delete_router(routers, session=None):
     neutron = get_neutron_client(session)
-    return neutron.delete_router(routers)
+    try:
+        return neutron.delete_router(routers)
+    except Exception as e:
+        utils.log_err(e)
 
 
 def list_subnet_pool(session=None):
     obj_subnetpool_list = list()
     neutron = get_neutron_client(session)
-    obj_subnetpool_list = neutron.list_subnetpools()
+    try:
+        obj_subnetpool_list = neutron.list_subnetpools()
+    except Exception as e:
+        utils.log_err(e)
+
     return obj_subnetpool_list
 
 
 def show_subnet_pool(subnetpool, session=None):
     neutron = get_neutron_client(session)
-    obj_subnetpools = neutron.show_subnetpool(subnetpool)
+    try:
+        obj_subnetpools = neutron.show_subnetpool(subnetpool)
+    except Exception as e:
+        utils.log_err(e)
     return obj_subnetpools
 
 
 def delete_subnet_pool(subnetpool, session=None):
     neutron = get_neutron_client(session)
-    return neutron.delete_subnetpool(subnetpool)
+    try:
+        return neutron.delete_subnetpool(subnetpool)
+    except Exception as e:
+        utils.log_err(e)
+
+
