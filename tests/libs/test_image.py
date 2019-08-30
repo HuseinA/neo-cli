e25b610efdd1f2a4294a7fd7699e85aad36f117e
azzamsa <azzam@azzamsa.com>
2019-08-30 18:52:54 +0700

    Add: black check to CI pipeline
    This will make every commit meet the project spesifications

--- tests/libs/test_image.py
+++ tests/libs/test_image.py
@@ -3,8 +3,8 @@ import mock
 import neo.libs.image
 from neo.libs import image
 
-class TestImage:
 
+class TestImage:
     def fake_client_list_images(self, session):
         fake_image = mock.Mock()
         fake_image.images.list.return_value = ["fake_img1", "fake_img2"]
@@ -12,9 +12,7 @@ class TestImage:
 
     def test_get_list(self, monkeypatch):
         monkeypatch.setattr(
-            neo.libs.image,
-            "get_image_client",
-            self.fake_client_list_images,
+            neo.libs.image, "get_image_client", self.fake_client_list_images
         )
         assert image.get_list("fake_session") == ["fake_img1", "fake_img2"]
 
@@ -25,8 +23,6 @@ class TestImage:
 
     def test_detail(self, monkeypatch):
         monkeypatch.setattr(
-            neo.libs.image,
-            "get_image_client",
-            self.fake_client_get_images,
+            neo.libs.image, "get_image_client", self.fake_client_get_images
         )
         assert image.detail("123") == "a detail"
